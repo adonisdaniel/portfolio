ecbc868aa232afd6d0e91863ecb89ee2414338f4
Adonis Mendoza <donimen1@gmail.com>
2025-11-28 15:56:21 -0400

    Fixes ion base

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://adonisdaniel.github.io/portfolio/',
-  base: '/', // Solo si NO es tu-usuario.github.io
+  site: 'https://adonisdaniel.github.io',
+  base: '/portfolio',
 });
